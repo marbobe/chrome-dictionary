3df79b2ebd62e0d61e051482d2d1c9788434ed8d
Mar <marbou96@hotmail.com>
2025-06-20 17:31:18 +0200

    first version japanese dictionary

--- popup.js
+++ popup.js
@@ -3,16 +3,23 @@ const input = document.getElementById("input-el");
 const definitionSpace = document.getElementById("p-el");
 const reminder = document.getElementById("reminder-btn");
 const tooEasy = document.getElementById("tooeasy-btn");
+const wait = document.getElementById("wait");
 let myWords = JSON.parse(localStorage.getItem("myWords")) || []; //cargar palabras anteriores si existen
 let learned = JSON.parse(localStorage.getItem("learned")) || []; //palabras aprendidas si existen
 
-async function showDefinition() {
-    const word = input.value.trim();
+
+
+async function showDefinition(wordParam) {
+    const word = wordParam || input.value.trim();
+
+    if (!word) {
+        document.getElementById("extra-btn").style.display = "none";
+        return;
+    };
+
+    wait.style.display = "block";
+    definitionSpace.style.display = "none";
     document.getElementById("extra-btn").style.display = "none";
-    definitionSpace.style.display = "block";
-
-    if (!word) return;
-
     try {
         const response = await fetch(`https://jisho.org/api/v1/search/words?keyword=${word}`)
 
@@ -22,7 +29,6 @@ async function showDefinition() {
             if (data.data.length === 0) {
                 throw new Error("Palabra no encontrada");
             }
-
             if (!myWords.includes(word) && !learned.includes(word)) {
                 myWords.push(word);
                 localStorage.setItem("myWords", JSON.stringify(myWords));
@@ -36,25 +42,35 @@ async function showDefinition() {
             const reading = japanese.reading || "(sin lectura)";
             const meanings = senses.english_definitions.join(", ");
 
-            definitionSpace.textContent = `${word} - ${kanji} [${reading}]: ${meanings}`;
-
+            definitionSpace.innerHTML = `<strong>${word}</strong> - ${kanji} [${reading}]: ${meanings}`;
+            wait.style.display = "none";
+            definitionSpace.style.display = "block";
             document.getElementById("extra-btn").style.display = "block";
         }
 
-
     } catch (error) {
-        definitionSpace.textContent = `Definition for the word: ${word} not found. Please try again and check the misspelling`;
+        document.getElementById("extra-btn").style.display = "none";
+        definitionSpace.innerHTML = `Definition for the word: <strong>${word}</strong> not found. Please try again and check the misspelling`;
         console.error(error);
+        definitionSpace.style.display = "block";
+        wait.style.display = "none";
     }
 }
 
-async function setReminder(myWords) {
-
-
-    input.value = "";
+async function setReminder() {
     definitionSpace.style.display = "none";
     document.getElementById("extra-btn").style.display = "none";
 
+    const randomWord = myWords[Math.floor(Math.random() * myWords.length)];
+
+    const min = 5 * 60 * 1000;
+    const max = 10 * 60 * 1000;
+    const delay = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    setTimeout(() => showDefinition(randomWord), delay);
+
+    input.value = "";
+    input.focus();
 }
 
 async function wordLearned() {
@@ -77,11 +93,38 @@ async function wordLearned() {
     document.getElementById("extra-btn").style.display = "none";
 
     input.focus();
+
+    getStars();
 }
 
-input.focus();
+async function getStars() {
+    const container = document.getElementById("container");
+    const score = document.getElementById("score");
+    const count = learned.length;
 
-search.addEventListener("click", showDefinition);
+    if (count < 10) {
+        container.style.display = "none";
+        return;
+    }
+
+    const stars = Math.min(5, Math.floor(count / 10));
+    const starsHTML = '<i class="fa-solid fa-star" style="margin: 5px"></i>'.repeat(stars);
+    let text = '';
+
+    if (count % 10 === 0 && count <= 50) {
+        text =
+            `<p>Congratulations you have learned ${count} words: <br>
+            ${learned.map(w => `<strong> ${w}</strong>`).join(', ')}</p>`;
+    }
+    text += `<i style="color: #FFBF00; font-size: 32px;">${starsHTML} </i>`;
+
+    score.innerHTML = text;
+    container.style.display = "block";
+}
+
+
+input.focus();
+getStars();
+search.addEventListener("click", () => { showDefinition() });
 reminder.addEventListener("click", setReminder);
 tooEasy.addEventListener("click", wordLearned);
-
